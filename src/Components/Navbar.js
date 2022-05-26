ffba93f97a786b317eca285706a7d2c1d20a8383
Ankit Bourasi <ankitbourasi0@gmail.com>
2022-05-26 11:56:17 +0530

    Frontend-Complete-withSaveEmployee-backend

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="bg-gray-800">
+    <div className='h-16 flex justify-center items-center' >
+      <p className='text-white font-bold text-xl'>
+        Employee Management System
+      </p>
+    </div>
+  </div>
+  )
+}
+
+export default Navbar
